7c086cf031e238b629aae41844524d0e226662ed
943432216 <35713053+943432216@users.noreply.github.com>
2018-07-13 10:16:32 +0800

    zhang
    对返回数据的字段做了限制

--- app/portal/Common.php
+++ app/portal/Common.php
@@ -4,19 +4,23 @@
  * [list_case description]
  * @param  string $year  [年份，返回当年发布的案例]
  * @param  string $limit [条数，限制数据量]
+ * @param  string $field [限制返回数据的字段]
  * @return [array]       [合作案例数据]
  */
-function list_case($year='', $limit='')
+function list_case($year='', $limit='', $field='')
 {
 	if (empty($year)) {
-		$data = \app\portal\model\PortalPostModel::all(function($query) use($limit) {
-			$query->limit($limit)->order('published_time desc');
+		$data = \app\portal\model\PortalPostModel::all(function($query) use($field,$limit) {
+			$query->field($field)
+			      ->limit($limit)
+			      ->order('published_time desc');
 		});
 	} else {
 		$begin = mktime(0,0,0,1,1,$year);
 		$end = mktime(0,0,0,1,1,++$year);
-		$data = \app\portal\model\PortalPostModel::all(function($query) use($begin,$end,$limit) {
-			$query->where('published_time', '>=', $begin)
+		$data = \app\portal\model\PortalPostModel::all(function($query) use($field,$begin,$end,$limit) {
+			$query->field($field)
+			      ->where('published_time', '>=', $begin)
 				  ->where('published_time', '<', $end)
 				  ->order('published_time desc')
 			      ->limit($limit);
