dbbf02dcacd7fa75efe8e421ec304f274f44ef22
943432216 <35713053+943432216@users.noreply.github.com>
2018-07-18 10:31:54 +0800

    zhang
    业务版块接口

---
+++ app/portal/controller/BusinessController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 品专官网 - 业务版块控制器
+ */
+
+namespace app\portal\controller;
+
+use cmf\controller\HomeBaseController;
+use think\DB;
+
+class BusinessController extends HomeBaseController {
+	public function index()
+	{
+		return $this->fetch('/business');
+	}
+
+	public function page()
+	{
+		$category = 5;
+		$page = 1;
+		$number = 7;
+		$begin = $number*($page - 1);
+
+		$data = DB::name('portal_post')->alias('a')
+									   ->join('portal_category_post b', 'a.id=b.post_id')
+								       ->where('b.category_id', $category)
+									   ->field('a.id,a.post_keywords,a.post_excerpt,a.more')		
+									   ->limit($begin, $number)
+									   ->select();
+		if ($data->isEmpty()) {
+			return json_encode(['error' => '没有相关数据'], JSON_UNESCAPED_UNICODE);
+		}
+		$data = $data->toArray();
+		foreach ($data as $k => $v) {
+			$data[$k]['more'] = cmf_get_image_url(json_decode($v['more'], true)['thumbnail']);
+		}
+		return json_encode($data, JSON_UNESCAPED_UNICODE);
+	}
+}
