435ca09bc5116613d2cb517137c45682cd1425dd
943432216 <35713053+943432216@users.noreply.github.com>
2018-08-13 17:00:22 +0800

    zhang

--- app/portal/controller/DspController.php
+++ app/portal/controller/DspController.php
@@ -13,8 +13,34 @@ class DspController extends HomeBaseController
 
 	public function dspRegister()
 	{
-		print_r($_POST);exit;
-		
+		print_r(json_encode($_POST));exit;
+
+
+		// $post = input('post.');
+
+		// $result = $this->validate($post, 'PortalDsp', [], true);
+
+		// if (true !== $result) {
+		// 	$this->result('400','注册失败', $result);
+		// }
+
+		// // halt(captcha_check($result['codes']));
+
+		// if(captcha_check($result['codes'])){
+		//  	//验证码正确
+		//  	$this->success('注册成功');
+		//  	return ['codes' => '200'];
+		//  	$insert_data = [
+		//  		'dsp_name' => $post['dsp_name'],
+		//  		'user_pwd' => $post['user_pwd'],
+		//  		'user_phone' => $post['user_']
+		//  	];
+		 	
+		// } else {
+		// 	//验证码不正确
+		// 	$this->error('注册失败');
+		// 	$this->result('400', '注册失败', ['codes'=>'验证码不正确']);
+		// }
 
 	}
 }
