a636313a17ecd42c8a157a459c3824a7c42d1b50
943432216 <35713053+943432216@users.noreply.github.com>
2018-08-15 11:24:35 +0800

    超
    跟新

--- public/themes/thinkpz/public/js/regular.js
+++ public/themes/thinkpz/public/js/regular.js
@@ -1,9 +1,12 @@
-function regs() {
-	var alts = null;
+(function (win) {
+	var alts = null,
+		nx = null;
+	var e = {};
 	var formInspect = {
 		//名字
 		username: function (el) {
 			var reg = /^[\u4e00-\u9fa5]{1,8}$/;
+			// var ele=$(el)
 			if (($(el).val() == '') || (reg.test($(el).val())) == false) {
 				alts = '姓名不能为空或请输入正确的姓名格式';
 				return alts;
@@ -14,7 +17,7 @@ function regs() {
 		//地址
 		site: function (el) {
 			var reg = /^(?=.*?[\u4E00-\u9FA5])[\dA-Za-z\u4E00-\u9FA5]+$/;
-			if (($(el).val() == '') || (reg.test($(el).val())) == false) {
+			if (($(''+el).val() == '') || (reg.test($(el).val())) == false) {
 				alts = '请输入正确的地址';
 				return alts
 			} else {
@@ -24,19 +27,20 @@ function regs() {
 		//手机
 		tel: function (el) {
 			var reg = /^1[34578]\d{9}$/;
-
-			if ($(el).val() == '') {
-				alts = '手机号不能为空';
-				return alts
-			} else if (reg.test($(el).val()) == false) {
-				alts = '请输入正确的11位手机号码';
-				return alts;
-			} else if ($(el).val().length < 11 || $(el).val().length > 11) {
-				alts = '请输入正确的11位手机号码';
-				return alts;
-			} else {
-				return true;
-			}
+			var ele=$(el);
+// 			if ($(el).val() == '') {
+// 				alts = '手机号不能为空';
+// 				return alts
+// 			} else if (reg.test($(el).val()) == false) {
+// 				alts = '请输入正确的11位手机号码';
+// 				return alts;
+// 			} else if ($(el).val().length < 11 || $(el).val().length > 11) {
+// 				alts = '请输入正确的11位手机号码';
+// 				return alts;
+// 			} else {
+// 				return true;
+// 			}
+			return el
 		},
 		//用户名
 		user: function (el) {
@@ -82,37 +86,45 @@ function regs() {
 				return true;
 			}
 		},
-		codes:function(el){
-			if($(el).val() == ''||$(el).val() == null){
+		codes: function (el) {
+			if ($(el).val() == '' || $(el).val() == null) {
 				return alts = '验证码不能为空';
-			}else{
+			} else {
 				return true;
 			}
 		}
 	}
-	var e = {}
-	var nx = null
-	if(arguments.length<=2){
-		nx = formInspect[arguments[1]](arguments[0]);
-	}else{
-		nx = formInspect[arguments[2]](arguments[1], arguments[0]);
-	}
-	e[arguments[arguments.length-1]]=nx;
-	$.each(e,function(a){
-		if(e[a]==true){
-			
-		}else{
-			var pL=$('#'+a).siblings('p').innerWidth();
-			var spanL=$('#'+a).parent('span').css('margin-left');
-			var spanT=$('#'+a).parent('span').position().top;
-			var spanM=$('#'+a).parent('span').css('margin-top')
-			var innH=$('#'+a).parent('span').innerHeight();
-			var innw=$('#'+a).innerWidth();
-			offsetT=parseFloat(spanT)+parseFloat(innH)+parseFloat(spanM)+5;
-			offsetL=parseFloat(pL)+parseFloat(spanL);
-			var hint='<b class="hint" style="left:'+offsetL+'px;top:'+offsetT+'px;width:'+innw+'px;">'+e[a]+'</b>';
-			$('.form').append(hint);
-			//弹出错误
+
+	function regs(index,value,callback) {
+		if (value=='string') {
+			nx = formInspect[index](value);
+		} else {
+			nx = formInspect[index](value[1], value[0]);
 		}
-	})
-}
+		e[index] = nx;
+		console.log(nx)
+		console.log(value)
+		$.each(e, function (a) {
+			if (e[a] == true) {
+				
+			} else {
+				console.log($('#' + a))
+				var pL = $('#' + a).siblings('p').innerWidth();
+				var spanL = $('#' + a).parent('span').css('margin-left');
+				var spanT = $('#' + a).parent('span').position().top;
+				var spanM = $('#' + a).parent('span').css('margin-top')
+				var innH = $('#' + a).parent('span').innerHeight();
+				var innw = $('#' + a).innerWidth();
+				offsetT = parseFloat(spanT) + parseFloat(innH) + parseFloat(spanM) + 5;
+				offsetL = parseFloat(pL) + parseFloat(spanL);
+				var hint = '<b class="hint" style="left:' + offsetL + 'px;top:' + offsetT + 'px;width:' + innw + 'px;">' + e[a] +
+					'</b>';
+				$('.form').append(hint);
+				$('#' + a).focus(function(){
+					$('.hint').remove();
+				})
+			}
+		})
+	}
+	win.regs = regs;
+})(window);
