6e888b7e9a5268005af6ffcb9a388fe1aaceea96
943432216 <35713053+943432216@users.noreply.github.com>
2018-08-15 16:35:50 +0800

    zhang

--- app/portal/controller/AdminMessageController.php
+++ app/portal/controller/AdminMessageController.php
@@ -13,4 +13,56 @@ class AdminMessageController extends AdminBaseController
 	{
 		return $this->fetch();
 	}
+
+	public function msgList()
+	{
+		$page = input('page', 1, 'intval');
+		$limit = input('limit', 10, 'intval');
+		$status = input('status', 0, 'intval');
+
+		$field = 'id,msg_name,msg_phone,msg_email,message,create_time,custom_ip,custom_city';
+		$where = [
+			'msg_status' => $status,
+		];
+		$user_dt = Db::table('pz_custom_msg')->field($field)->where($where)->limit($limit*($page - 1),$limit)->select()->toArray();
+		$count = Db::table('pz_custom_msg')->where($where)->count();
+
+		foreach ($user_dt as $key => $value) {
+			$user_dt[$key]['create_time'] = date('Y-y-d H:i:s', $value['create_time']);
+		}
+
+		$array = [
+			'code' => 0,
+			'msg' => 'success',
+			'count' => $count ,
+			'data' => $user_dt,
+		];
+		return $array;
+	}
+
+	//删除单个留言
+	public function delOne()
+	{
+		$id = input('post.id');
+		$db = Db::table('pz_custom_msg')->where(['id' => $id])->update(['msg_status' => -1]);
+		if ($db) {
+			return ['code' => 200, 'msg' => '删除成功'];
+		} else {
+			return ['code' => 404, 'msg' => '删除失败，请重试'];
+		}
+	}
+
+	//批量删除留言
+	public function delMany()
+	{
+		
+		$post = input('post.ids');
+		$del_result = Db::table('pz_custom_msg')->where(['id' => ['in', $post]])->update(['msg_status' => -1]);
+
+		if ($del_result) {
+			$this->result('', 1, '删除成功');
+		} else {
+			$this->result('', 0, '删除失败');
+		}
+	}
 }
--- app/portal/controller/MessageController.php
+++ app/portal/controller/MessageController.php
@@ -9,6 +9,19 @@ class MessageController extends HomeBaseController
 {
 	public function takeOver()
 	{
+		$msg = input('post.');
 		
+		$city = get_city();
+		$msg['create_time'] = time();
+		$msg['custom_ip'] = get_client_ip();
+		$msg['custom_city'] = empty($city) ? '无法定位' : $city['region'] . $city['city'];
+
+		$db_insert = Db::table('pz_custom_msg')->insert($msg);
+
+		if ($db_insert) {
+			$this->result('', 200, '留言成功');
+		} else {
+			$this->result('', 400, '留言失败，请重试或联系客服');
+		}
 	}
 }
